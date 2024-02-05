95172b9d044e0bd61781f3d723dc38d5dd03dc68
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-02-05 16:55:24 +0000

    daily activity

---
+++ Kickoff warmup.py
@@ -0,0 +1,5 @@
+# i think a computer program is codes thaT YOU CREATE AND PROGRAM
+#my favorite food is oat meal the steps to make it is 
+gert your oats and put it in the stove for 10 minetus 
+then mix your cinnamon with the oats 
+then mix your syrup with it then you have oat meal                                
