773f34ca9f2406ca812b5062745ea77c8b0fa02e
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-02-16 17:37:27 +0000

    questions quiz feb16

---
+++ jamirquiz.py
@@ -0,0 +1,62 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user?
+#i would use print()
+
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+#i would use input()
+
+#3. Explain what compelity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+#abstract in coding is to show strings in different coding ways and compleity is the amount of time 
+#the abstract manages the complecity by deleting any uneededinformation  
+#5. What is a variable?  
+#something that stores data varibles. A+B could be 5+10
+#6. Write a function in Python that when called will print out 
+# your name. 
+#print(f'my name is'(jamir)
+
+#7. What is a function definition and a function call?
+#a block code which only works when it is called
+
+#8. Describe what each symbol means?
+
+# = // single equal it means ture or false 
+# == // double equal it checks if the values or equal or not 
+# != // exclaimation equal it means it is not equal
+
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+#carmel case, snake case, pascal case,
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+#when we compare x==y
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+#x>0 and x<10
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+
+# Bonus : What does the type() function do? it returnes any stored data 
