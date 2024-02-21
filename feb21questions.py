088a11c1749074ac1a327189728a53b7a45521ea
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-02-21 17:47:02 +0000

    feb21questions

---
+++ feb21questions.py
@@ -0,0 +1,20 @@
+#Based on the video you just watched, in your own words, describe what is the Socratic Method.
+#awnsering questions and after awnsering asking more questions
+#Why do you think it is important to asks questions in software engineering?
+#to fully understand the topic. and be sore understamding of the topic
+#Based on the video you just watched, in your own words, what do you think a data broker is?
+#providing organisation to keep user personal information as safe as possible.
+#Why do you think companies collect and sell people's data?
+#to use their data in other illegal activites
+#Based on the video you just watched, what does structural systemic racism mean? Provide 1 (one) example of structural system racism.
+#policles only enforced on certin recial groups because of the struccture of society.exapmle  two black men aressted for a crime they didn't comit in 2010 and 20
+#Why do you think it is important to build algorithms where there exists racial context?
+#because certin recial group might not understand the way the algorithm speaks or because of color comfort.
+#Why do you think it is important to have people of color involved in the development of these AI algorithms and systems?
+i think it might important because of the society they have an impact on along with the impact on young colored people. G9iving them reasons to look up 
+1#
+
+   
+
+
+
---
+++ questions warmup.py
@@ -0,0 +1,7 @@
+#create a varible that contains a lsit of things that go in a laundry room. you should have at least 5
+#mylist = ["soap",detergint",baking soda", hanger, hamper"]
+
+
+#3 def reverseName():
+username = input('what is your name? ')[::-1]
+print(userName)
