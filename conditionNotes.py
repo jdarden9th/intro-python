00de45e34528e0d57644bcfc37c4bff331e226c9
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-02-28 17:04:53 +0000

    feb27questions/notes

--- conditionNotes.py
+++ conditionNotes.py
@@ -1,2 +1,9 @@
 #question
 #awnser:i would first prompt the user to enter their passcode then i would retrive the passcode enterend by the user the i would compare the entered passcode with the predefined passaccode'got the money' ifthe enterd passcode mathches the prefinded passcode, grant access or if the passcode does not match the predenfeinned passcode, deby access. once implemented this system will effectively check the passcode entered by users at the door and grant or deny access accordingly. 
+def entrancepasscode(passcode):
+    actualpasscode = 'get_this_money'
+    usercode = input('please enter user code. ')
+    print('acess Granted.')
+else:
+print(Acess granted.'')
+entrancepasscode()
