4c08f304db66c3538b4850ee10fac6d9daf487fb
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-02-28 16:42:11 +0000

    conditions/notesquestionfen27

---
+++ conditionNotes.py
@@ -0,0 +1,2 @@
+question
+awnser:i would first prompt the user to enter their passcode then i would retrive the passcode enterend by the user the i would compare the entered passcode with the predefined passaccode'got the money' ifthe enterd passcode mathches the prefinded passcode, grant access or if the passcode does not match the predenfeinned passcode, deby access. once implemented this system will effectively check the passcode entered by users at the door and grant or deny access accordingly. 
---
+++ feb23questions.py
@@ -0,0 +1,5 @@
+#function definition and function call 
+                                                      
+
+
+
