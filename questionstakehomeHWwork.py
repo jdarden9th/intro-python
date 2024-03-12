25d05e54d7337e1ca7de200b7d45555af25c4f4b
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-03-12 16:59:52 +0000

    question

---
+++ questionstakehomeHWwork.py
@@ -0,0 +1,11 @@
+# Programming Questions
+# How do you think technology has influenced and
+# shaped how you learn in school? Do you think it's
+# been positive or negative? Please explain why 
+# by providing examples, written in 4 to 5 complete 
+# sentences. 
+#i think it shaped me because everything now is technology so i can better understanding of it since im learing it. i think its been poistive because since everything is technology we would now how to do more things by our self in the future.i also think it is good because it might be something you like.
+# What are some ways you think technology can help 
+# people learn? Please explain why by providing examples,
+# written in 4 to 5 complete sentences. 
+#it could be helpful since everything is tecnology. if you sick you can order stuff now you could also buy house on phones.
