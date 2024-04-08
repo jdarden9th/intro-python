9e4dad33ea4c5eeadac45f0e0106d8e61616ec56
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-04-08 15:51:24 +0000

    classwork

---
+++ aysncworkapril8.py
@@ -0,0 +1,22 @@
+# Async Activity April 8th, 2024
+
+# 1. In your own words, describe what a while loop is?
+a loop is a code the repates 
+#2. Create a function that uses a while loop to determine if a user has typed in 
+# the the correct word guess. If the user types in the wrong word, your program 
+# should inform them that their guess was inccorect and to try again, but if the
+# user guesses the word correctly, your program shoul tell the user they have 
+# guessed correctly and have won the game, stopping the loop.
+
+def word_guess(word):
+    correct_word ="correct"
+    wrong_word + "incorrect"
+    guess = ""
+    while guess != correct_word:
+        if guess == wrong_wrong:
+            print("your guess was incorrect.try again")
+        else:
+            print("congratulations, you guessed the word correctly! you won the game")
+            break
+        guess = input("enter your guess")
+
