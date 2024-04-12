11dfce845ad4d70c027dc400138baf7e7fe08ca6
jdarden9th <158079818+jdarden9th@users.noreply.github.com>
2024-04-12 16:26:58 +0000

    warmup

---
+++ april12warmup.py
@@ -0,0 +1,29 @@
+#a for loop is a loop that doesnt last long and a while long is a loop that last long.
+    
+#i would use a for loop for grade and a while loop for merchindice
+for i in range(1,21):
+    if i % 2 == 0:
+        print(f"{i}is an even number.")
+    else:
+        print(f"{i}is an odd number.")
+
+1 is am odd number.
+2 is an even number.
+3 is an odd number
+4 is an even number
+5 is an odd number
+6 is an even number
+7 is an odd number
+8 is an even number 
+9 is an odd number
+10 is an even number
+11 is an odd number
+12 is an even number.
+13 is an odd number.
+14 is an even number.
+15 is an odd number.
+16 is an even number. 
+17 is an odd number.
+18 is an even number.
+19 is an odd number.
+20 is an even number.
